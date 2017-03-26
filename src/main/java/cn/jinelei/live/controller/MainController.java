815f376da379656aab6363132ad3b558f3d55327
jinelei <jinelei@163.com>
2017-03-26 11:20:18 +0000

    add freemarker template

--- src/main/java/cn/jinelei/live/controller/MainController.java
+++ src/main/java/cn/jinelei/live/controller/MainController.java
@@ -2,7 +2,11 @@ package cn.jinelei.live.controller;
 
 import com.sun.istack.internal.logging.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.logging.Level;
 
@@ -15,10 +19,18 @@ import java.util.logging.Level;
 public class MainController {
 
     private static final Logger logger = Logger.getLogger(MainController.class);
-    @RequestMapping(value = "/index")
+
+    @RequestMapping(value = {"/index", "/"})
     public String index(){
         logger.log(Level.INFO,"index info");
         return "index";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(Model model){
+        model.addAttribute("msg","this msg from server");
+        logger.log(Level.INFO, model.toString());
+        return "test";
     }
 
 
